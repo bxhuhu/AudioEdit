c85bd0c3d59010f181d3bede82c19dd423e1e9dc
bxhuhu <1091064779@qq.com>
2021-12-04 01:34:22 +0800

    项目初始化提交K

---
+++ app/src/main/java/com/cree/audio_edit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cree.audio_edit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main_layout)
+    }
+}
