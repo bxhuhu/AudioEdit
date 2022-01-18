fcb102d9e76ac222b1478a22e31ce12e043d62e5
陆勇江 <1091064779@qq.com>
2022-01-18 22:34:52 +0800

    拆分service组件,添加config配置

---
+++ AudioEditService/src/main/java/com/cree/audioservice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cree.audioservice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //dev 开放版本分支
+    }
+}
