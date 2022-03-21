5f9f9c7403ae3bf10b53e06fea3d3f8c51cd40a0
bxhuhu <1091064779@qq.com>
2022-03-21 22:46:10 +0800

    项目基本配置修改上传,抽取config配置常量

--- AudioEditHMI/src/main/java/com/cree/audio_edit_hmi/MainActivity.kt
+++ AudioEditHMI/src/main/java/com/cree/audio_edit_hmi/MainActivity.kt
@@ -1,12 +1,11 @@
-package com.cree.audio_edit
+package com.cree.audio_edit_hmi
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.act_main_layout)
-
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ AudioEditService/src/main/java/com/cree/audio_edit_service/AudioEditServiceActivity.kt
@@ -0,0 +1,11 @@
+package com.cree.audio_edit_service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioEditServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
